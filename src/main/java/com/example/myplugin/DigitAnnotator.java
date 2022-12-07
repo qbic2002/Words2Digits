ae3c23dcfbe844f53f7d5e6bfe53127c6c880eca
golov <golovinmax28@gmail.com>
2022-12-08 02:10:14 +0300

    feat: add custom effect

--- src/main/java/com/example/myplugin/DigitAnnotator.java
+++ src/main/java/com/example/myplugin/DigitAnnotator.java
@@ -5,9 +5,13 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.markup.EffectType;
+import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiIdentifier;
+import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
 
 public class DigitAnnotator implements Annotator {
@@ -19,10 +23,15 @@ public class DigitAnnotator implements Annotator {
 
         TextRange textRange = TextRange.from(element.getTextOffset(), element.getTextLength());
         if (element.getText().contains("one") || element.getText().contains("two") || element.getText().contains("three") || element.getText().contains("four") || element.getText().contains("five") || element.getText().contains("six") || element.getText().contains("seven") || element.getText().contains("eight") || element.getText().contains("nine") || element.getText().contains("zero")) {
-//            holder.newSilentAnnotation(HighlightSeverity.ERROR)
-//                    .range(textRange).textAttributes(DefaultLanguageHighlighterColors.INVALID_STRING_ESCAPE).highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL).create();
+            TextAttributes textAttributes = DefaultLanguageHighlighterColors.NUMBER.getDefaultAttributes();
+            textAttributes.setErrorStripeColor(JBColor.blue);
+            textAttributes.setEffectType(EffectType.WAVE_UNDERSCORE);
+            textAttributes.setEffectColor(JBColor.blue);
+
+            TextAttributesKey tempUnknownNumber = TextAttributesKey.createTempTextAttributesKey("UNKNOWN NUMBER", textAttributes);
+            TextAttributesKey unknownNumber = TextAttributesKey.createTextAttributesKey("UNKNOWN NUMBER", tempUnknownNumber);
             holder.newAnnotation(HighlightSeverity.ERROR, "Digit-like string")
-                    .range(textRange).textAttributes(DefaultLanguageHighlighterColors.NUMBER)
+                    .range(textRange).textAttributes(unknownNumber)
                     .withFix(new FixDigit(textRange, element.getText()))
                     .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL).create();
         }
